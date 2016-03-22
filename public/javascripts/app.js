c84deb32dd3d271a7c83a44733b5f3bd0e80347f
Cache Staheli <cache@byu.edu>
2016-03-22 03:15:10 +0000

    not working, but all the code is there.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,8 +11,13 @@ angular.module('comment', [])
       {title:'Comment 5', upvotes:3}
     ];
     $scope.addComment = function() {
-      $scope.comments.push({title:$scope.formContent,upvotes:0});
-      $scope.formContent='';
+      if($scope.formContent === '') { return; }
+      console.log("In addComment with "+$scope.formContent);
+      $scope.create({
+        title: $scope.formContent,
+        upvotes: 0,
+      });
+      $scope.formContent = '';
     };
     $scope.incrementUpvotes = function(comment) {
       comment.upvotes += 1;
@@ -23,5 +28,22 @@ angular.module('comment', [])
       angular.copy(data, $scope.comments);
     });
   };
+  $scope.create = function(comment) {
+    return $http.post('/comments', comment).success(function(data){
+      $scope.comments.push(data);
+    });
+  };  
+
+    $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+        .success(function(data){
+          console.log("upvote worked");
+          comment.upvotes += 1;
+        });
+    };
+
+    $scope.incrementUpvotes = function(comment) {
+      $scope.upvote(comment);
+    };	
   }
 ]);
